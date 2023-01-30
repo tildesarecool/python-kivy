c287ecfd9e1c1288b9aeb8ac6213c685e0a1f366
Tildes <tildesare.cool@gmail.com>
2023-01-30 16:00:10 -0600

    A window dynamically sized and displaying buttons.

--- main.py
+++ main.py
@@ -5,7 +5,21 @@
 # courtesy of freeCodeCamp
 
 from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+
+class BoxLayoutExample(BoxLayout):
+    # pass
+    # instead of using kivy file
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # unlike in kivy file must use = here
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        self.add_widget(b1)
+        self.add_widget(b2)
+
 
 class MainWidget(Widget):
     pass
